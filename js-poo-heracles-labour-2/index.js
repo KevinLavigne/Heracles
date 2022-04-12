14616845fa35bcd168c4915fa7f6a13667546d40
KevinLavigne <lavigne.kevin02@gmail.com>
2022-04-12 16:38:35 +0200

    armor penetration added on Figter Constructor who reduce defence on getdefence. props lifePotion added on fighter Constructor who be used on function  usePotion who heal hercule if his life fall below 50% of maxlife. add eachFightReward function who give a random stats bonus a each ennemy beaten add console.log on different element to understood what appening on each round

--- js-poo-heracles-labour-2/index.js
+++ js-poo-heracles-labour-2/index.js
@@ -1,60 +1,104 @@
-const {Fighters} = require("./src/fighter")
-const Shield = require("./src/Shield")
-const Weapon = require("./src/Weapon")
+const { Fighters } = require('./src/fighter');
+const Shield = require('./src/Shield');
+const Weapon = require('./src/Weapon');
 // First Labour : Heracles vs Nemean Lion
-const sword = new Weapon("sword", 10)
+const sword = new Weapon('wooden broadsword', 10);
 
-const shield = new Shield("shield", 10)
+const shield = new Shield('wooden round shield', 10);
 
-const heracles = new Fighters("🧔 Heracles",100, 20 , 6, null , null,0,0)
+const heracles = new Fighters('🧔 Hercule', 100, 20, 6, null, null, 0, 0);
 const monster = [
-new Fighters("🦁 Nemean Lion",100, 11 , 13,null,null),
-new Fighters("🐗 Erymanthian Boar",100, 25, 12,null,null),
-new Fighters("The Hydra",200,25,15,null,null,5,0)
-]
-let victory =0
-function result(fighter1 , fighter2){
-    if(fighter1.life <= 0){
-        console.log(`${fighter1.name} is dead 💀`)
-        console.log(`${fighter2.name} Wins 🏆`)
-    }else{
-        console.log(`${fighter2.name} is dead 💀`)
-        console.log(`${fighter1.name} Wins 🏆`)
-        victory++
-        reward()
-        }
+	new Fighters('🦁 Nemean Lion', 100, 11, 13, null, null),
+	new Fighters('🐗 Erymanthian Boar', 100, 25, 12, null, null),
+	new Fighters('The Hydra', 200, 25, 15, null, null, 5, 0),
+];
+let victory = 0;
+function result(fighter1, fighter2) {
+	if (fighter1.life <= 0) {
+		console.log(`${fighter1.name} is dead 💀`);
+		console.log(`${fighter2.name} Wins 🏆`);
+	} else {
+		console.log(`${fighter2.name} is dead 💀`);
+		console.log(`${fighter1.name} Wins 🏆`);
+		victory++;
+		eachFightReward();
+		reward();
+	}
 }
-let round = 0
-function brawl(fighter1 ,fighter2){
-console.log(`Here was the fight of ${fighter1.name}  ⚔️ ${fighter2.name}`)
-console.log(`${fighter1.name} : ${fighter1.life}/${fighter1.maxLife}💙 ⚔️ ${fighter2.name} : ${fighter2.life}/${fighter2.maxLife}💙`)
-    while (fighter1.life >= 1 || fighter2.life >= 1 ) {
-        round++
-        console.log(`round #${round}`)
-        fighter1.fight(fighter1,fighter2)
-        if (fighter2.life > 0){
-            fighter2.fight(fighter2 ,fighter1)
-        }
-        if ((fighter1.life <= 0) || (fighter2.life <=0)){
-            result(fighter1 ,fighter2)
-            break
-        }
-    } 
+const halfMaxLife =(attacker)=>{
+	return (attacker.maxLife / 2)
+} 
+let round = 0;
+function brawl(fighter1, fighter2) {
+	console.log(`Here was the fight of ${fighter1.name}  ⚔️ ${fighter2.name}`);
+	console.log(
+		`${fighter1.name} : ${fighter1.life}/${fighter1.maxLife}💙 ⚔️ ${fighter2.name} : ${fighter2.life}/${fighter2.maxLife}💙`
+	);
+	while (fighter1.life >= 1 || fighter2.life >= 1) {
+		round++;
+		console.log(`round #${round}`);
+		fighter1.fight(fighter1, fighter2);
+		if (fighter1.life < (halfMaxLife(fighter1)) && (fighter1.lifePotion > 0)) drinkPotion(fighter1);
+		if (fighter2.life > 0) {
+			fighter2.fight(fighter2, fighter1);
+		}
+		if (fighter1.life <= 0 || fighter2.life <= 0) {
+			result(fighter1, fighter2);
+			break;
+		}
+	}
 }
-const reward=() => {
-    heracles.life = (Math.min(100, heracles.life + 50 ));
-    if (victory === 1){
-        heracles.weapon = sword
-        heracles.shield = shield
-    }
-    if (victory === 2){
-        heracles.lifeSteal =+ 25
-    }
-    if (victory < 3){
-    brawl(heracles, monster[victory])
-    }
-    if (victory == 3){ 
-    console.log(`${heracles.name} defeat all enemys`)
-    }
+const reward = () => {
+	heracles.life = Math.min(100, heracles.life + 50);
+	if (victory === 1) {
+		heracles.weapon = sword;
+		heracles.shield = shield;
+		console.log(
+			`God's decide to reward ${heracles.name} with an ${heracles.weapon.name} and an ${heracles.shield.name}`
+		);
+	}
+	if (victory === 2) {
+		heracles.lifeSteal = +25;
+		console.log(
+			`Ares give you the ability of use the blood of your oponent to restore your vitality. ${heracles.name} now have ${heracles.lifeSteal}% of vampirisme`
+		);
+	}
+	if (victory < 3) {
+		brawl(heracles, monster[victory]);
+	}
+	if (victory === 3) {
+		console.log(`${heracles.name} defeat all enemys`);
+	}
+};
+function eachFightReward(attacker) {
+	const rand = Math.round(Math.random() * 3 + 1);
+	if (rand === 1) {
+		heracles.maxLife += 20;
+	heracles.life = Math.min(heracles.maxLife, heracles.life + 20);
+		console.log(
+			`${heracles.name} gain 20 Max life point. Is actual life is ${heracles.life}/${heracles.maxLife}`
+		);
+	}
+	if (rand === 2) {
+		heracles.strenght += 5;
+		console.log(
+			`${heracles.name} gain 5 strenght point. Is actual strenght is ${heracles.strenght}`
+		);
+	}
+	if (rand === 3) {
+		heracles.dexterity += 5;
+		console.log(
+			`${heracles.name} gain 5 dexterity point. Is actual strenght is ${heracles.dexterity}`
+		);
+	}
 }
-brawl(heracles,monster[victory])
+function drinkPotion(attacker) {
+	attacker.usePotion(attacker);
+	console.log(
+		`${attacker.name} drink an healing potion and restore ${
+			halfMaxLife(attacker)
+		}, he now have ${attacker.life}`
+	);
+	attacker.lifePotion = attacker.lifePotion - 1;
+}
+brawl(heracles, monster[victory]);
--- js-poo-heracles-labour-2/src/fighter.js
+++ js-poo-heracles-labour-2/src/fighter.js
@@ -1,62 +1,103 @@
-const Weapon = require("./Weapon")
-const Shield = require("./Shield")
-
+const Weapon = require('./Weapon');
+const Shield = require('./Shield');
 
 class Fighters {
-    constructor(name,maxLife, strenght , dexterity , weapon , shield,regen,lifeSteal, life, critDmg){
-        this.name = name;
-        this.maxLife = maxLife;
-        this.strenght = strenght;
-        this.dexterity = dexterity;
-        this.weapon = weapon;
-        this.shield = shield;
-        this.regen = regen;
-        this.lifeSteal = lifeSteal;
-        this.life = maxLife;
-        this.critDmg = (1 + (this.strenght/100));
-    }
-    
-    crit(){
-        if ( this.dexterity > Math.round(Math.random()* 100 +1 )){
-            console.log(`${this.name} infict a critical hit`)
-            return this.critDmg
-        } else {
-            return 1
-        }
-    }
-    getRandom(max){
-       return 1 + Math.floor(Math.random()* max)
-    }
-    fight(attacker,defender){
-        defender.life = Math.max(0,defender.life - Math.max(0,(this.getAttack(attacker) - this.getDefence(defender)))) ;
-        console.log(`${attacker.name} ⚔️ ${defender.name} 💙 ${defender.name}: ${defender.life}/${defender.maxLife}`);
-        if (attacker.lifeSteal >= 1){
-            attacker.stealLife(attacker)
-            console.log(`${attacker.name} steal ${this.lifeStealedTemp} life point 💙`)
-        }
-        if (attacker.regen >= 1){
-            attacker.regeneration(attacker)
-            console.log(`${attacker.name} self restore ${attacker.regen} ${attacker.life}/${attacker.maxLife}💙`)
-        }
-    }
-    getDefence(defender){
-        this.defenceTemp = defender.dexterity + (defender.shield == null ? 0 : defender.shield.protection)
-        return this.defenceTemp
-    }
-    defenceTemp = 0
-    getAttack(attacker){
-       this.attackTemp = (attacker.getRandom(attacker.strenght) +(attacker.weapon == null? 0: attacker.weapon.damage)+ 1)
-       return this.attackTemp
-    }
-    attackTemp = 0
-    stealLife(attacker){
-        this.lifeStealedTemp =Math.round(Math.max(0,(this.attackTemp - this.defenceTemp)) * attacker.lifeSteal /100)
-        return attacker.life = Math.min(attacker.maxLife, (attacker.life + this.lifeStealedTemp))
-    }   
-    lifeStealedTemp = 0
-    regeneration(attacker){
-        attacker.life = Math.min(attacker.maxLife, (attacker.life + attacker.regen))
-    }
-}
- module.exports={Fighters}
+	constructor(
+		name,
+		maxLife,
+		strenght,
+		dexterity,
+		weapon,
+		shield,
+		regen,
+		lifeSteal,
+		armorPiercing
+	) {
+		this.name = name;
+		this.maxLife = maxLife;
+		this.strenght = strenght;
+		this.dexterity = dexterity;
+		this.weapon = weapon;
+		this.shield = shield;
+		this.regen = regen;
+		this.lifeSteal = lifeSteal;
+		this.armorPiercing = null;
+		this.life = maxLife;
+		this.critDmg = 1 + this.strenght / 100;
+		this.lifePotion = 3;
+	}
 
+	crit() {
+		if (this.dexterity > Math.round(Math.random() * 100 + 1)) {
+			console.log(`${this.name} infict a critical hit`);
+			return this.critDmg;
+		} else {
+			return 1;
+		}
+	}
+	getRandom(max) {
+		return 1 + Math.floor(Math.random() * max);
+	}
+	fight(attacker, defender) {
+		defender.life = Math.max(
+			0,
+			defender.life -
+				Math.max(0, this.getAttack(attacker) - this.getDefence(defender, attacker))
+		);
+		console.log(
+			`${attacker.name} ⚔️ ${defender.name} 💙 ${defender.name}: ${defender.life}/${defender.maxLife}`
+		);
+		if (attacker.lifeSteal >= 1) {
+			attacker.stealLife(attacker);
+			console.log(
+				`${attacker.name} steal ${this.lifeStealedTemp} life point 💙`
+			);
+		}
+		if (attacker.regen >= 1) {
+			attacker.regeneration(attacker);
+			console.log(
+				`${attacker.name} self restore ${attacker.regen} ${attacker.life}/${attacker.maxLife}💙`
+			);
+		}
+	}
+	getDefence(defender, attacker) {
+		this.defenceTemp = Math.max(0,
+			defender.dexterity -
+			(attacker.armorPiercing === null ? 0 : attacker.armorPiercing) +
+			(defender.shield == null ? 0 : defender.shield.protection));
+		return this.defenceTemp;
+	}
+	defenceTemp = 0;
+	getAttack(attacker) {
+		this.attackTemp =
+			attacker.getRandom(attacker.strenght) +
+			(attacker.weapon == null ? 0 : attacker.weapon.damage) +
+			1;
+		return this.attackTemp;
+	}
+	attackTemp = 0;
+	stealLife(attacker) {
+		this.lifeStealedTemp = Math.round(
+			(Math.max(0, this.attackTemp - this.defenceTemp) * attacker.lifeSteal) /
+				100
+		);
+		return (attacker.life = Math.min(
+			attacker.maxLife,
+			attacker.life + this.lifeStealedTemp
+		));
+	}
+	lifeStealedTemp = 0;
+	regeneration(attacker) {
+		attacker.life = Math.min(attacker.maxLife, attacker.life + attacker.regen);
+		console.log(
+			`${attacker.name} regenerate ${attacker.regen} life point, he now have ${attacker.life}/${attacker.maxLife}`
+		);
+	}
+	usePotion(attacker) {
+		attacker.life = Math.min(
+			attacker.maxLife,
+			attacker.life + attacker.maxLife * 0.5
+		);
+	}
+}
+module.exports = { Fighters };
